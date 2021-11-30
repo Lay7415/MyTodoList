cd346a09e1ff9a9f5145c2c0fd11d7bc2c85eb45
Lay7531 <ieye7531@gmail.com>
2021-12-01 01:12:30 +0600

    I created app with local starage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Card from './components/Card';
 import Form from './components/Form';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import Userlist from './components/UserList';
 
 function App() {
   const [user,setUser] = useState([])
+  const [save,setSave] = useState("false")
+  
+  const savedData = JSON.parse(localStorage.getItem("users"))
   const onFormDataHundler =(UserData)=> {
     setUser((prevlist) => {
       return [
@@ -13,11 +16,20 @@ function App() {
         {name: UserData.username, password: UserData.password,id: UserData.id}
       ]
     })
+    setSave("true")
   }
+  useEffect(() => {
+    if(save === "true") {
+      localStorage.setItem("users", JSON.stringify(user))
+      setSave("false")
+    }
+    setUser([...savedData || []])
+  }, [save])
   const reListHundler =(newList) => {
     console.log(user.id)
     console.log(newList)
     setUser(user.filter(user => user.id !== newList))
+    setSave("true")
   }
   return (
     <div className="App">
